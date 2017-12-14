66c673e74b8af17389d11f4963d9070bedfe4c80
Matt Emmerich <matt.emmerich@gmail.com>
2017-12-13 16:00:49 -0800

    Add files via upload

---
+++ js/splash.js
@@ -0,0 +1,9 @@
+// setTimeout(function() {
+//     document.getElementById("loader").className = "wheelLeft";
+// }, 5000);
+//
+//
+//
+// setTimeout(function() {
+//     document.getElementById("loader").className = "wheelLeft";
+// }, 5000);
